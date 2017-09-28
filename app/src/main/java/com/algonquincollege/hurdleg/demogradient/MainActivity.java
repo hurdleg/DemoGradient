1acc38dba62bff6a3bab3e516af98ff6dea252f7
Gerald Hurdle <hurdleg@algonquincollege.com>
2017-09-27 20:57:51 -0400

    Initial commit.

---
+++ app/src/main/java/com/algonquincollege/hurdleg/demogradient/MainActivity.java
@@ -0,0 +1,13 @@
+package com.algonquincollege.hurdleg.demogradient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
